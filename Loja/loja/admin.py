dc4a68ecdc6dc2878128b4d28010672a81dafb2f
Heitor de França Barboza <20231011110036@ifrn.local>
2025-05-06 15:32:24 -0300

    Capítulo 4

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
 # Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+class ProdutoAdmin(admin.ModelAdmin):
+    search_fields = ('Produto',)
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
