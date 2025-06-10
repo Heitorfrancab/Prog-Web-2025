309fb8a2fd245bae5a8cccd3a8cf1d0a5a9a4246
Heitor de França Barboza <20231011110036@ifrn.local>
2025-06-10 15:49:13 -0300

    Primeira metade do capítulo 7

--- Loja/loja/views/HomeView.py
+++ Loja/loja/views/HomeView.py
@@ -1,3 +1,11 @@
+from loja.models import Produto
 from django.shortcuts import render
 def home_view(request):
-    return render(request, template_name='home/home.html', status=200)
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='home/home.html', context=context, status=200)
